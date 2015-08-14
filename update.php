ac316dec51397e4f2760d740eb49a0594a3a0194
Tran Anh Duc <anhduc.itspecialist@gmail.com>
2015-08-14 11:12:51 +0700

    sửa nhiều lắm

---
+++ update.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Acc</th>
+            <th>Pass</th>
+            <th>Lv</th>
+        </tr>
+        <tr>
+            <td><input type="text" name="name"></td>
+            <td><input type="text" name="acc"></td>
+            <td><input type="password" name="pass"></td>
+            <td><select><option>1</option><option>2</option></select></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
